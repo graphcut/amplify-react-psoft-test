96db117664d63710ab4926853be75c3b6cb3753b
graphcut <graphcut@gmail.com>
2022-12-15 14:34:53 +1300

    Changes for V2-IDP

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "amplifyreactpsofttes5077f39e": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
     "api": {
         "amplifyreactpsofttes": {
             "GraphQLAPIKeyOutput": "string",
